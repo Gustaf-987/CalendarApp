86fc5e72a893718ab181811c682996b9bda1a5a5
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-12 23:07:43 -0600

    update

--- Assets/script.js
+++ Assets/script.js
@@ -1,87 +1,29 @@
-var date = moment().format("MMM Do YY");
-var time = Date($.now());
-var saveButtons = $(".saveBtn");
-var inputBox = $(".textarea");
-
-
+moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
-// for (let i = 0; i < inputBox.length; i++) {
-//     var timeNow = parseInt($(inputBox[i]).attr("id"));
-//     var compareHour = parseInt(moment().format("HH"));
-
-//     if (timeNow < compareHour){
-//         $(inputBox[i]).addclass(".past");
-//     } else if (timeNow > compareHour){
-//         ($(inputBox[i]).addclass(".future"))
-//     } else ($(inputBox[i]).addclass(".present"))
+for (let i = 1; i <= 9; i++) {
+    $("#G" + i).val(localStorage.getItem(i))
     
-    
-// }
+}
 
 
-$("#btn9").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#9").val();
-    localStorage.setItem("btn9", JSON.stringify(text9));
-    
-    console.log(JSON.parse(localStorage.getItem(text9)));
+$(".saveBtn").on("click", function(){
+    localStorage.setItem(this.id, $("#G" +this.id).val());
 })
 
 
-$("#btn10").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#10").val();
-    localStorage.setItem("btn10", text9);
-})
-
-$("#btn11").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#11").val();
-    localStorage.setItem("btn11", text9);
-})
-
-
-$("#btn12").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#12").val();
-    localStorage.setItem("btn12", text9);
-})
-
-
-$("#btn13").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#13").val();
-    localStorage.setItem("btn13", text9);
-})
-
-
-$("#btn14").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#14").val();
-    localStorage.setItem("btn14", text9);
-})
-
-
-$("#btn15").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#15").val();
-    localStorage.setItem("btn15", text9);
-})
-
-
-$("#btn16").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#16").val();
-    localStorage.setItem("btn16", text9);
-})
-
-
-$("#btn17").on("click", function(event){
-    event.preventDefault;
-    var text9 = $("#17").val();
-    localStorage.setItem("btn17", text9);
-})
-
-
+var currentTime = moment().hour() -8;
+  for (let i = 1; i <= 9; i++) {
+     
+    if (i === currentTime){
+        $("#G" + i).attr("class", "textarea present")
+    }
+    else if (i < currentTime){
+        $("#G" + i).attr("class", "textarea past")
+    }
+    else if (i > currentTime){
+        $("#G" + i).attr("class", "textarea future")
+    }
+     
+ }
     
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Homework5_CalendarApp
 Calendar Application
+
+A calendar app to scheudle your work day. Enter tasks into the hour slots and hit save to store it. 
+
+![Screenshot]()
